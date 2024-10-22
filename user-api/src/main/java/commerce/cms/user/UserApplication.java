81546a52f5a3745eb2e819ee4526501394bb1ec4
sosa7753 <sosa7753@naver.com>
2024-10-22 16:44:34 +0900

    [feat-0304] 토큰 및 고객 검증 필터 추가 - Filter interface를 구현해서 HTTP 프로토콜에 의한 token 검증 및 id, email로 고객 조회 - 필터를 쓰기 위한 ServetComponentScan 추가

--- user-api/src/main/java/commerce/cms/user/UserApplication.java
+++ user-api/src/main/java/commerce/cms/user/UserApplication.java
@@ -2,10 +2,11 @@ package commerce.cms.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
+@ServletComponentScan
 @EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
