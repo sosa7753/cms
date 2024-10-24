5e0ac261c19a0759645e4969d1b905c73b85a91b
sosa7753 <sosa7753@naver.com>
2024-10-24 16:43:50 +0900

    [feat-0306] 셀러 토큰 필터 추가

---
+++ user-api/src/main/java/commerce/cms/user/config/filter/SellerFilter.java
@@ -0,0 +1,40 @@
+package commerce.cms.user.config.filter;
+
+import commerce.cms.domain.config.JwtAuthenticationProvider;
+import commerce.cms.domain.domain.common.UserVo;
+import commerce.cms.user.service.seller.SellerService;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+
+@WebFilter(urlPatterns = "/seller/*")
+@RequiredArgsConstructor
+public class SellerFilter implements Filter {
+
+  private final JwtAuthenticationProvider provider;
+  private final SellerService sellerService;
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+      throws IOException, ServletException {
+
+    HttpServletRequest req = (HttpServletRequest) request;
+    String token = req.getHeader("X-AUTH-TOKEN");
+    if (!provider.validateToken(token)) {
+      throw new ServletException("Invalid token");
+    }
+
+    UserVo vo = provider.getUserVo(token);
+    sellerService.findByIdAndEmail(vo.getId(), vo.getEmail()).orElseThrow(
+        () -> new SecurityException("Invalid access")
+    );
+    chain.doFilter(request, response); // 필터를 다음 필터나 서블릿으로 넘김.
+
+  }
+}
