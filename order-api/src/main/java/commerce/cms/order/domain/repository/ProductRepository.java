18b60125d1467f86b7324679ad058de8f1fb95d4
sosa7753 <sosa7753@naver.com>
2024-11-10 23:46:13 +0900

    [feat-0406] Product List 조회시 아이템도 조회 메서드 추가

--- order-api/src/main/java/commerce/cms/order/domain/repository/ProductRepository.java
+++ order-api/src/main/java/commerce/cms/order/domain/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package commerce.cms.order.domain.repository;
 
 import commerce.cms.order.domain.model.Product;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
@@ -16,4 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, Long>, Product
   @EntityGraph(attributePaths = {"productItems"}, type = EntityGraphType.LOAD)
   Optional<Product> findWithProductItemsById(Long id);
 
+  @EntityGraph(attributePaths = {"productItems"}, type = EntityGraphType.LOAD)
+  List<Product> findAllByIdIn(List<Long> ids);
 }
